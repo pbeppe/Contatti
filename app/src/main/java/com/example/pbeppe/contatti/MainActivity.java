146511cc4dfa748b01f2bec719c4079e5e127a27
pbeppe <palmisano.giuseppe@live.com>
2018-02-25 16:16:34 +0100

    Fine Layout

--- app/src/main/java/com/example/pbeppe/contatti/MainActivity.java
+++ app/src/main/java/com/example/pbeppe/contatti/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.pbeppe.contatti;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listview;
     String[] nomi={"Cosimo","Agostino","Antonio","Federico","Gianni","Maria","Mara","Francesca","Martina","Eva"};
     int[] foto={R.drawable.cosimo,R.drawable.agostino,R.drawable.antonio,R.drawable.federico,R.drawable.gianni,
             R.drawable.maria,R.drawable.mara,R.drawable.francesca,R.drawable.martina,R.drawable.eva};
@@ -12,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listview=(ListView)findViewById(R.id.lstcontatti);
     }
 }
