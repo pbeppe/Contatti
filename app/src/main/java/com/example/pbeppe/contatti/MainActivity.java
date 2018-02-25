592cc7ee546eb5aead7db2743ff5f131d84f2638
pbeppe <palmisano.giuseppe@live.com>
2018-02-25 15:26:54 +0100

    Inizio

--- app/src/main/java/com/example/pbeppe/contatti/MainActivity.java
+++ app/src/main/java/com/example/pbeppe/contatti/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] nomi={"Cosimo","Agostino","Antonio","Federico","Gianni","Maria","Mara","Francesca","Martina","Eva"};
+    int[] foto={R.drawable.cosimo,R.drawable.agostino,R.drawable.antonio,R.drawable.federico,R.drawable.gianni,
+            R.drawable.maria,R.drawable.mara,R.drawable.francesca,R.drawable.martina,R.drawable.eva};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
